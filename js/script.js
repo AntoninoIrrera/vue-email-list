8df0b3e60de2968d814524eecdabfc0ccc8a66e0
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-21 14:21:46 +0100

    prendo 10 email dall API fornito e li stampo nel DOM

--- js/script.js
+++ js/script.js
@@ -6,12 +6,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-           
+            arrayEmail : [],
+            count: 1,
         }
     },
     methods: {
 
+        getEmailFromList(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
 
+                this.arrayEmail.push(response.data.response);
+
+            })
+        }
+
+
+    },
+    created(){
+        while (this.count <= 10) {
+            this.getEmailFromList()
+            this.count++;
+        }
 
     }
 }).mount("#app")
